b833d8d4c5c9ec53e426c81dc56886794e605f05
Shriram Azhagarasan <azhagarasan.s@northeastern.edu>
2024-12-07 22:25:16 -0500

    updated filterRoutes

--- backend/src/routes/filterRoutes.js
+++ backend/src/routes/filterRoutes.js
@@ -4,7 +4,10 @@ import * as filterController from "../controllers/filterController.js";
 const router = express.Router();
 
 router.route('/')
-  .get(filterController.searchEvents);
+  .get(filterController.getAllEvents);
+
+router.route("/datefilters")
+  .get(filterController.getEventsByDateRange);
 
 export default router; // Use `export default` instead of `module.exports`
 
